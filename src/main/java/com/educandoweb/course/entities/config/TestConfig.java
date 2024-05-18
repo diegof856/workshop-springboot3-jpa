913baf4edec83027dbd2f69fc549603bcbf661c3
diegof856 <diegofelipe1025@gmail.com>
2024-05-17 23:39:11 -0300

    Enums, Entidade pedido e datas com instant

--- src/main/java/com/educandoweb/course/entities/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/entities/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.entities.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.enums.OrderStatus;
+import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UserRepository;
 
 @Configuration
@@ -15,13 +19,21 @@ import com.educandoweb.course.repositories.UserRepository;
 public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
-
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Diego Felipe", "diegofelipe@gmail.com","(81) 9 95871-5851", "12445747");
 		User u2 = new User(null, "maria", "maria@gmail.com","(81) 9 94541-5851", "12445ada7");
 		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT,u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID,u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID,u1);
+		
 		userRepository.saveAll(Arrays.asList(u1,u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
 	
 	
