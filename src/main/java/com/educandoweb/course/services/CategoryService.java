84c66998cd123ca6f56ef643675e0e31457d63c6
diegof856 <diegofelipe1025@gmail.com>
2024-05-17 23:52:14 -0300

    Entidade Category

---
+++ src/main/java/com/educandoweb/course/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	@Autowired
+	private CategoryRepository repositoty;
+
+	public List<Category> findAll() {
+		return repositoty.findAll();
+	}
+
+	public Category findById(Long id) {
+		Optional<Category> obj = repositoty.findById(id);
+		return obj.get();
+	}
+}
